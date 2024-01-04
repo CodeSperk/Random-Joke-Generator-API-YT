aedd597153ec1af58fb6bf7cf81fecdd3e56ee2f
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-01-04 21:18:19 +0600

    Lodaning effect added

--- script/script.js
+++ script/script.js
@@ -13,9 +13,17 @@ const options = {
 const apiUrl = "https://api.api-ninjas.com/v1/dadjokes?limit=1"
 
 async function getJoke(){
+  jokeEl.innerText = "Updating...";
+
+  btnEl.disabled = true;
+  btnEl.innerText = "Loading...";
+
   const response =await fetch(apiUrl, options);
   const data =await response.json();
   jokeEl.innerText = data[0].joke;
+
+  btnEl.disabled = false;
+  btnEl.innerText = "Tell me a Joke";
 }
 
 btnEl.addEventListener("click", getJoke)
